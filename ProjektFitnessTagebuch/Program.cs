c43432bb5f337ab1a25943fc10c41a09442dde36
Melwin Weber <melwin.weber0910@gmail.com>
2025-10-16 11:37:32 +0200

    Login und Benutzerdaten

--- ProjektFitnessTagebuch/Program.cs
+++ ProjektFitnessTagebuch/Program.cs
@@ -3,9 +3,41 @@
     internal class Program
     {
         static void Main(string[] args)
-        {      
-            // Test
-            Console.WriteLine("Hello, World!");
+        {
+            // Variablen für Benutzereingaben
+            string geschlecht;
+            int alter;
+            double gewicht, größe;
+            int aktivitätslevel;
+
+            // Begrüßung und Eingabeaufforderungen
+            Console.WriteLine("Willkommen zum Fitness-Tagebuch!");
+            Console.WriteLine("Wir berechnen deinen täglichen Kalorien- und Nährstoffbedarf.");
+            Console.WriteLine(); // Leerzeile für bessere Lesbarkeit 
+
+            // Eingabe des Geschlechts, Alters, Gewichts, Größe und Aktivitätslevels
+            Console.Write("Bitte gib dein Geschlecht ein (m/w): ");
+            geschlecht = Console.ReadLine();
+
+            Console.Write("Bitte gib dein Alter ein (in Jahren): ");
+            alter = int.Parse(Console.ReadLine());
+
+            Console.Write("Bitte gib dein Gewicht ein (in kg): ");
+            gewicht = double.Parse(Console.ReadLine());
+
+            Console.Write("Bitte gib deine Größe ein (in cm): ");
+            größe = double.Parse(Console.ReadLine()); 
+
+            Console.WriteLine();
+            Console.WriteLine("Aktivitätslevel:(Wähle eine Zahl aus)");
+            Console.WriteLine("1 - Wenig oder keine Bewegung");
+            Console.WriteLine("2 - Leichte Bewegung (1-3 Tage pro Woche)");
+            Console.WriteLine("3 - Mäßige Bewegung (3-5 Tage pro Woche)");
+            Console.WriteLine("4 - Intensive Bewegung (6-7 Tage pro Woche)");
+            Console.Write("Deine Wahl: ");
+            aktivitätslevel = int.Parse(Console.ReadLine());
+
+
         }
     }
 }
